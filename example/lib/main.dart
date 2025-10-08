fbc50354a5adaa040776280ce1d106caf70c0e62
nirmal01465 <cs1210588@iitd.ac.in>
2025-10-08 06:16:05 +0530

    Add backend directory

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:audio_tagger/audio_tagger.dart';
@@ -27,15 +28,11 @@ class _MyAppState extends State<MyApp> {
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
     try {
-      platformVersion =
-          await AudioTagger.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
+      platformVersion = Platform.operatingSystemVersion;
+    } catch (e) {
       platformVersion = 'Failed to get platform version.';
     }
-
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
